e6cbd452ea06c924114c3a5d088a79c0ab885402
lee <mike.hyungi.lee@gmail.com>
2019-11-17 19:03:45 +0900

    lesson 4

--- app/src/main/java/hyungil/example/a40_phone_portrait_layout/MainActivity.kt
+++ app/src/main/java/hyungil/example/a40_phone_portrait_layout/MainActivity.kt
@@ -1,7 +1,10 @@
 package hyungil.example.a40_phone_portrait_layout
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  *
@@ -13,5 +16,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ロゴを丸いイメージにする
+        // make the logo as rounded image.
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+//        rounded.isCircular = true
+        logo.setImageDrawable(rounded)
+
     }
 }

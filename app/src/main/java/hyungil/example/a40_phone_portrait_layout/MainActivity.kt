a36f9e5429a4bf984cea25535fa50ff6d95dfb84
lee <mike.hyungi.lee@gmail.com>
2019-11-17 13:07:53 +0900

    [update]ActionbarのTheme（NoActionBar）と色を変更

--- app/src/main/java/hyungil/example/a40_phone_portrait_layout/MainActivity.kt
+++ app/src/main/java/hyungil/example/a40_phone_portrait_layout/MainActivity.kt
@@ -3,6 +3,11 @@ package hyungil.example.a40_phone_portrait_layout
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ *
+ * DevProfile Project
+ *
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
